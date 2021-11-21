b0f5c0addaffb954cd2513fb6a0acc8c50ac05ce
MurilloFlorent <murillo.florentin1@gmail.com>
2021-11-21 13:48:19 -0300

    Criando conexao com o banco

--- backend/src/Backend.hs
+++ backend/src/Backend.hs
@@ -1,10 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase, GADTs #-}
 module Backend where
 
 import Common.Route
 import Obelisk.Backend
+import Database.PostgreSQL.Simple
+import Data.Text
+import Obelisk.Route
+import Snap.Core
+import Control.Monad.IO.Class (liftIO)
+import qualified Data.Aeson as A
+
+getConn :: ConnectInfo
+getConn = ConnectInfo "ec2-35-173-207-244.compute-1.amazonaws.com"
+                      5432
+                      "ejgrpwsfuuxycg"
+                      "daf4205c7d32bd190cb6eaed384791239857c859625054d5194ef53d6d5f3136"
+                      "d6es6d556n98vj"
+
+migration :: Query
+migration = "CREATE TABLE IF NOT EXISTS cliente (id SERIAL PRIMARY KEY, nome TEXT NOT NULL)"
 
 backend :: Backend BackendRoute FrontendRoute
 backend = Backend
-  { _backend_run = \serve -> serve $ const $ return ()
-  , _backend_routeEncoder = fullRouteEncoder
-  }
+  { _backend_run = \serve -> do
+      dbcon <- connect getConn
+      serve $ do
+        \case
+            BackendRoute_Cliente :/ () -> do
+              Just nome <- A.decode <$> readRequestBody 2000
+              liftIO $ do
+                    execute_ dbcon migration
+                    execute dbcon "INSERT INTO cliente (nome) VALUES (?)" [nome :: Text]
+              modifyResponse $ setResponseStatus 200 "OK"
+            _ -> return ()
+, _backend_routeEncoder = fullRouteEncoder
+}
--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeApplications, ScopedTypeVariables #-}
 
 module Frontend where
 
@@ -24,21 +24,41 @@ import Common.Route
 -- This runs in a monad that can be run on the client or the server.
 -- To run code in a pure client or pure server context, use one of the
 -- `prerender` functions.
-data Pagina = Principal | Categoria | Produtos
+data Pagina = Principal | Categoria | Produtos | Pagina4
+
+getPath :: T.Text
+getPath = renderBackendRoute checFullREnc $ BackendRoute_Cliente :/ ()
+
+nomeRequest :: T.Text -> XhrRequest T.Text
+nomeRequest s = postJson getPath  (Cliente s)
+
+req :: ( DomBuilder t m
+        , Prerender js t m
+        ) => m ()
+req = do
+  inputEl <- inputElement def
+  (submitBtn,_) <- el' "button" (text "Inserir")
+  let click = domEvent Click submitBtn
+  let nm = tag (current $ _inputElement_value inputEl) click
+  _ :: Dynamic t (Event t (Maybe T.Text)) <- prerender
+      (pure never)
+      (fmap decodeXhrResponse <$> performRequestAsync (nomeRequest <$> nm))
+  return ()
 
 clickli :: DomBuilder t m => Pagina -> T.Text -> m (Event t Pagina)
 clickli p t = do
   (ev, _) <- elAttr' "li" ("class" =: "nav-item") (elAttr "a" ("href" =: "#" <> "class" =: "nav-link") (text t))
   return ((\_ -> p) <$> domEvent Click ev)
 
-currPag :: (DomBuilder t m, PostBuild t m, MonadHold t m) => Pagina -> m ()
+currPag :: (DomBuilder t m, PostBuild t m, MonadHold t m, Prerender js t m) => Pagina -> m ()
 currPag p = 
   case p of
     Principal -> blank
     Categoria -> catpag
     Produtos -> prodpag
+    Pagina4 -> req
 
-homepag :: (DomBuilder t m , PostBuild t m, MonadHold t m) => m ()
+homepag :: (DomBuilder t m , PostBuild t m, MonadHold t m, Prerender js t m) => m ()
 homepag = do
   pagina <- el "div" menu
   el "ul" $ do
@@ -48,13 +68,13 @@ homepag = do
     el "li" (text "Rodrigo Hailer")
   dyn_ $ currPag <$> pagina
 
-catpag :: (DomBuilder t m , PostBuild t m, MonadHold t m) => m ()
+catpag :: (DomBuilder t m , PostBuild t m, MonadHold t m, Prerender js t m) => m ()
 catpag = do
   el "h1" (text "Pagina Categorias") 
   el "span" (text "Pagina em construcao")
 
 
-prodpag :: (DomBuilder t m , PostBuild t m, MonadHold t m) => m ()
+prodpag :: (DomBuilder t m , PostBuild t m, MonadHold t m, Prerender js t m) => m ()
 prodpag = do
   el "h1" (text "Pagina Produtos") 
   el "span" (text "Pagina em construcao")
@@ -73,7 +93,8 @@ menu = do
             p1 <- clickli Principal "Teste da Home"
             p2 <- clickli Categoria "Categoria"
             p3 <- clickli Produtos "Produtos"
-            return (leftmost [p1,p2,p3])
+            p4 <- clickli Pagina4 "Insercao banco"
+            return (leftmost [p1,p2,p3,p4])
   holdDyn Principal evs
 
 
